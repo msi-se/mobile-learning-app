f58f79060070ac38fca54fc972c1fffa7dc4209f
TobiTgl <tobias_813@t-online.de>
2023-12-10 19:08:02 +0100

    simple basic auth

--- backend/src/main/java/de/htwg_konstanz/mobilelearning/controller/AuthController.java
+++ backend/src/main/java/de/htwg_konstanz/mobilelearning/controller/AuthController.java
@@ -1,17 +1,33 @@
 package de.htwg_konstanz.mobilelearning.controller;
 
+import java.util.Base64;
+
+import org.jboss.resteasy.reactive.RestHeader;
+
 import de.htwg_konstanz.mobilelearning.models.auth.User;
 import de.htwg_konstanz.mobilelearning.services.auth.LdapHtwg;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthController {
 
     @GET
-    public User auth() throws Exception {
-        LdapHtwg ldapUser = new LdapHtwg();
-        return ldapUser.doLogin("", "");
+    public Response auth(@RestHeader("Authorization") String authorization) throws Exception {
+        String encodedString = authorization.split(" ")[1];
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        String username = decodedString.split(":")[0];
+        String password = decodedString.split(":")[1];
+
+        try {
+            LdapHtwg ldapUser = new LdapHtwg();
+            User user = ldapUser.doLogin(username, password);
+            return Response.ok(user).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
     }
     
 }
