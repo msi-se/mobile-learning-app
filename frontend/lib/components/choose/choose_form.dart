154de93c7e4e6c33a55a99eedea0b7ff73125451
Fabian <fabian.klimpel2002@gmail.com>
2024-01-04 18:22:26 +0100

    typo

--- frontend/lib/components/choose/choose_form.dart
+++ frontend/lib/components/choose/choose_form.dart
@@ -27,7 +27,7 @@ class _ChooseFormState extends State<ChooseForm> {
     var forms = feedbackOrQuiz == "Feedback" ? widget.course.feedbackForms : [];
 
     return SliverLayout(
-      collapsable: false,
+      collapsable: true,
       expandedTitleScale: 1,
       title: (percentage) {
         return Align(
