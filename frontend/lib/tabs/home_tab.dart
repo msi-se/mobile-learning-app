84f07a8d53eee0b52731eb5030d729790ab569e7
polymarv <marvin-berglen@gmx.de>
2024-01-14 15:23:33 +0100

    added basic verison of dashboard

--- frontend/lib/tabs/home_tab.dart
+++ frontend/lib/tabs/home_tab.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/components/dashboard_card.dart';
 import 'package:frontend/theme/assets.dart';
+
+Widget _buildColumn(String title, String value) {
+  return Column(
+    children: [
+      SizedBox(height: 20),
+      Text(
+        title,
+        style: const TextStyle(
+          fontSize: 15,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      Text(
+        value,
+        style: const TextStyle(fontSize: 30, color: Colors.grey),
+        textAlign: TextAlign.center,
+      ),
+    ],
+  );
+}
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key, required this.title});
@@ -23,6 +45,9 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     if (loading) {
       return const Center(
         child: CircularProgressIndicator(),
@@ -30,23 +55,67 @@ class _HomeTabState extends State<HomeTab> {
     }
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              widget.title,
-            ),
-            SizedBox(
-              height: 300.0,
-              child: Center(
-                child: Image.asset(htwgLogo),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                children: [
+                  MyCard(
+                      title: 'Events',
+                      cardColor: Colors.grey,
+                      imageName: events),
+                  MyCard(
+                      title: 'Mensa',
+                      cardColor: Colors.yellow,
+                      imageName: mensa),
+                  MyCard(
+                    title: 'LSF',
+                    cardColor: Colors.orange,
+                    imageName: calendar,
+                  ),
+                  MyCard(
+                    title: 'Noten',
+                    cardColor: Colors.green,
+                    imageName: analytics,
+                  ),
+                ],
+                shrinkWrap: true,
               ),
-            ),
-          ],
+              Container(
+                decoration: BoxDecoration(
+                    color: colors.inverseSurface,
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                width: screenWidth,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildColumn('Gegebene Feedbacks', '12'),
+                        _buildColumn('Absolvierte Quizze', '55'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildColumn('Ø Quiz-Position', '4'),
+                        _buildColumn('Alle Feedbacks ', '1923'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-// 
