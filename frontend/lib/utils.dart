ece96bb176d296cc57ed1cbccccf7e3e1210432c
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-01-15 23:27:54 +0100

    backend url

--- frontend/lib/utils.dart
+++ frontend/lib/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 String getBackendUrl({String protocol = 'http'}) {
   // final String? backendUrl = Platform.environment['BACKEND_URL']; // later maybe
   if (defaultTargetPlatform == TargetPlatform.android) {
-    return '$protocol://10.0.2.2:8080';
+    return '$protocol://loco.in.htwg-konstanz.de/api';
   }
-  return '$protocol://localhost:8080';
+  return '$protocol://loco.in.htwg-konstanz.de/api';
 }
