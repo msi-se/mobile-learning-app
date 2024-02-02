dea85489ecb573ac7536e8d45bf820021e8c9892
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-02-02 18:43:43 +0100

    added jwt mocking (not tested yet)

--- backend/src/test/java/de/htwg_konstanz/mobilelearning/services/CourseServiceTest.java
+++ backend/src/test/java/de/htwg_konstanz/mobilelearning/services/CourseServiceTest.java
@@ -9,18 +9,24 @@ import de.htwg_konstanz.mobilelearning.models.Course;
 import de.htwg_konstanz.mobilelearning.models.auth.UserRole;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.jwt.Claim;
+import io.quarkus.test.security.jwt.JwtSecurity;
+
 import jakarta.inject.Inject;
 
 @QuarkusTest
 public class CourseServiceTest {
-    
+
     @Inject
     private CourseService courseService;
 
     @Test
     @TestSecurity(user = "testUser", roles = { UserRole.PROF, UserRole.STUDENT })
+    @JwtSecurity(claims = {
+            @Claim(key = "email", value = "user@gmail.com")
+    })
     public void testGetAllCourses() {
-        
+
         // get all courses
         List<Course> courses = courseService.getCourses();
 
