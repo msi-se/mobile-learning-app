d44f57ef8f5614e1ad429bfa1aeac64fdcfd9d69
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-02-19 17:12:03 +0100

    jvm builder

--- frontend/lib/utils.dart
+++ frontend/lib/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 String getBackendUrl({String protocol = 'http'}) {
   // final String? backendUrl = Platform.environment['BACKEND_URL']; // later maybe
   if (defaultTargetPlatform == TargetPlatform.android) {
-    return '$protocol://10.0.2.2:8080';
+    return '$protocol://10.0.2.2/api';
   }
-  return '$protocol://localhost:8080';
+  return '$protocol://localhost/api';
 }
