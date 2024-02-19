7fedae9f9e4df716bf124bd2e8f4b19863fc7bb0
johannesbrandenburger <johannes0709@icloud.com>
2024-02-19 18:26:34 +0100

    variable backend url in frontend

--- frontend/lib/utils.dart
+++ frontend/lib/utils.dart
@@ -2,8 +2,19 @@ import 'package:flutter/foundation.dart';
 
 String getBackendUrl({String protocol = 'http'}) {
   // final String? backendUrl = Platform.environment['BACKEND_URL']; // later maybe
-  if (defaultTargetPlatform == TargetPlatform.android) {
-    return '$protocol://10.0.2.2/api';
-  }
-  return '$protocol://localhost/api';
+
+  // get the current domain (when running in a browser)
+  final String domain = Uri.base.host;
+
+  // if the domain is localhost, use the local backend
+  if (domain == 'localhost') {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return '$protocol://10.0.2.2/8080';
+    }
+    return '$protocol://localhost/8080';
+
+    // if the domain is not localhost, use the deployed backend
+  } else {
+    return '$protocol://$domain/api';
+  }  
 }
