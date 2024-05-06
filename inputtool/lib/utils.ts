568dceb2cef8b03194d7ffca1bac3b6638ee21d0
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-05-06 23:54:14 +0200

    only check every 10 min if jwt is invalid

--- inputtool/lib/utils.ts
+++ inputtool/lib/utils.ts
@@ -15,6 +15,29 @@ export async function hasValidJwtToken(): Promise<boolean> {
     return false;
   }
 
+  // check if the token is still valid (client-side)
+  const isTokenExpired = (jwtToken: String) => Date.now() >= (JSON.parse(atob(jwtToken.split('.')[1]))).exp * 1000;
+  if (isTokenExpired(jwtToken)) {
+    // console.error(`Token is expired.`);
+    // console.error(`JWT: ${jwtToken}`);
+    toast.error("Token is expired. Please login again.");
+    localStorage.removeItem("jwtToken");
+    return false;
+  }
+
+  // if the token is not expired, check if it is still valid but only every 10 minutes
+  const lastCheck = localStorage.getItem("lastCheck");
+  if (lastCheck) {
+    const lastCheckTime = parseInt(lastCheck);
+    const currentTime = Date.now();
+    if (currentTime - lastCheckTime < 10 * 60 * 1000) {
+      // console.log(`Last check was less than 10 minutes ago.`);
+      return true;
+    }
+  }
+
+  localStorage.setItem("lastCheck", Date.now().toString());
+
   // check if the token is still valid
   const BACKEND_URL = await getBackendUrl();
 
