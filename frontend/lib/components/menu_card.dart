1f216f221c160ec354f8c383d72ac7266a5fd8ca
Tobi Tögel <71888952+TobiTgl@users.noreply.github.com>
2024-05-07 13:59:49 +0200

    change to card with icon and like button

--- frontend/lib/components/menu_card.dart
+++ frontend/lib/components/menu_card.dart
@@ -16,7 +16,8 @@ class MenuCard extends StatefulWidget {
   State<MenuCard> createState() => _MenuCardState();
 }
 
-class _MenuCardState extends State<MenuCard> with SingleTickerProviderStateMixin {
+class _MenuCardState extends State<MenuCard>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _opacityAnimation;
 
@@ -27,7 +28,8 @@ class _MenuCardState extends State<MenuCard> with SingleTickerProviderStateMixin
       vsync: this,
       duration: const Duration(milliseconds: 500),
     );
-    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
       parent: _animationController,
       curve: Curves.easeIn,
     ));
@@ -36,42 +38,80 @@ class _MenuCardState extends State<MenuCard> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return FadeTransition(
       opacity: _opacityAnimation,
       child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xFFD9E5EC),
+        padding: const EdgeInsets.all(2),
+        child: Card(
+          // Replace Container with Card
+          color: colors.onPrimary,
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
           ),
+          elevation: 2,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  widget.title,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                padding: const EdgeInsets.all(5),
+                child: ListTile(
+                  leading: Icon(
+                    widget.title == 'Seezeit-Teller'
+                        ? Icons.restaurant
+                        : widget.title == 'hin&weg'
+                            ? Icons.arrow_circle_down
+                            : widget.title == 'KombinierBar'
+                                ? Icons.bar_chart
+                                : widget.title == 'Pasta'
+                                    ? Icons.paste
+                                    : widget.title == 'Pasta vegetarisch'
+                                        ? Icons.paste
+                                        : widget.title == 'Sättigung 1'
+                                            ? Icons.house
+                                            : widget.title == 'Sättigung 2'
+                                                ? Icons.house_siding_rounded
+                                                : widget.title == 'Gemüse 1'
+                                                    ? Icons.view_agenda
+                                                    : widget.title == 'Salat 1'
+                                                        ? Icons.save_alt_rounded
+                                                        : Icons
+                                                            .no_meals_rounded, // Default icon
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  widget.description,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: widget.rowData.map((data) => data.build()).toList(),
+                  title: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      widget.title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Text(widget.description),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: widget.rowData
+                              .map((data) => data.build())
+                              .toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: InkWell(
+                    onTap: () {
+                      print("like");
+                    },
+                    child: const Icon(Icons.favorite_border),
+                  ),
                 ),
               ),
             ],
