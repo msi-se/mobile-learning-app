0f09daf1da6e74aff1d6ca1d86a5c41628097497
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-06-08 21:59:34 +0200

    stress test

--- backend/src/test/java/de/htwg_konstanz/mobilelearning/services/quiz/socket/LiveQuizSocketTest.java
+++ backend/src/test/java/de/htwg_konstanz/mobilelearning/services/quiz/socket/LiveQuizSocketTest.java
@@ -1,26 +1,27 @@
 package de.htwg_konstanz.mobilelearning.services.quiz.socket;
 
-import static io.restassured.RestAssured.given;
-
 import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import de.htwg_konstanz.mobilelearning.Helper;
-import de.htwg_konstanz.mobilelearning.SocketClient;
 import de.htwg_konstanz.mobilelearning.MockMongoTestProfile;
 import de.htwg_konstanz.mobilelearning.MockUser;
+import de.htwg_konstanz.mobilelearning.SocketClient;
 import de.htwg_konstanz.mobilelearning.models.Course;
 import de.htwg_konstanz.mobilelearning.models.Result;
 import de.htwg_konstanz.mobilelearning.models.quiz.QuizForm;
 import de.htwg_konstanz.mobilelearning.services.CourseService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import static io.restassured.RestAssured.given;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import jakarta.inject.Inject;
@@ -1526,5 +1527,181 @@ public class LiveQuizSocketTest {
             Assertions.fail(e.getMessage());
         }
     }
+
+
+    @Test
+    public void stressTest() throws InterruptedException, DeploymentException, IOException {
+
+        Integer AMOUNT_OF_STUDENTS = 50;
+
+        // create & get course
+        List<Course> courses = Helper.createCourse();
+        Course course = courses.get(0);
+
+        // get course and quiz form id
+        String courseId = course.getId().toString();
+        String formId = course.getQuizForms().get(0).getId().toString();
+
+        // create 1 prof and 500 students
+        MockUser prof = Helper.createMockUser("Prof");
+        List<MockUser> students = new ArrayList<>();
+        given().header("Authorization", "Bearer " + prof.getJwt()).when().get("/course").then().statusCode(200);
+        
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            MockUser student = Helper.createMockUser("Student" + i);
+            students.add(student);
+
+            // call the get courses endpoint for each user to update the course-user relation
+            Thread.sleep(100);
+            given().header("Authorization", "Bearer " + student.getJwt()).when().get("/course").then().statusCode(200);
+        }
+
+        // create a websocket client for the prof and the students
+        SocketClient profClient = new SocketClient();
+        List<SocketClient> studentClients = new ArrayList<>();
+        for (MockUser student : students) {
+            SocketClient studentClient = new SocketClient();
+            studentClients.add(studentClient);
+            Thread.sleep(100);
+        }
+
+        // connect the prof to the quiz form
+        Session profSession = ContainerProvider.getWebSocketContainer().connectToServer(
+            profClient,
+            URI.create("ws://localhost:8081/course/" + courseId + "/quiz/form/" + formId + "/subscribe/" + prof.getId() + "/" + prof.getJwt())
+        );
+        Thread.sleep(100);
+        Assertions.assertTrue(profSession.isOpen());
+
+        // set the form status to "WAITING" and check if it was set
+        profClient.sendMessage("""
+            {
+                "action": "CHANGE_FORM_STATUS",
+                "formStatus": "WAITING"
+            }
+        """);
+        Thread.sleep(100);
+        Assertions.assertEquals("WAITING", courseService.getCourse(courseId).getQuizForms().get(0).getStatus().toString());
+
+        Thread.sleep(1000);
+
+        // make the participate requests for the students and connect them to the quiz form
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            given().header("Authorization", "Bearer " + students.get(i).getJwt()).pathParam("courseId", courseId).pathParam("formId", formId).body("alias-student-" + i).when().post("/course/{courseId}/quiz/form/{formId}/participate").then().statusCode(200);
+            Thread.sleep(100);
+        }
+
+        Thread.sleep(5000);
+
+        Assertions.assertEquals(AMOUNT_OF_STUDENTS, courseService.getCourse(courseId).getQuizForms().get(0).getParticipants().size());
+
+        // connect the students to the quiz form
+        List<Session> studentSessions = new ArrayList<>();
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            Session studentSession = ContainerProvider.getWebSocketContainer().connectToServer(
+                studentClients.get(i),
+                URI.create("ws://localhost:8081/course/" + courseId + "/quiz/form/" + formId + "/subscribe/" + students.get(i).getId() + "/" + students.get(i).getJwt())
+            );
+            studentSessions.add(studentSession);
+            Thread.sleep(100);
+            Assertions.assertTrue(studentSession.isOpen());
+        }
+
+        // change the form status to "STARTED" and check if it was set
+        profClient.sendMessage("""
+            {
+                "action": "CHANGE_FORM_STATUS",
+                "formStatus": "STARTED"
+            }
+        """);
+        Thread.sleep(5000);
+        Assertions.assertEquals("STARTED", courseService.getCourse(courseId).getQuizForms().get(0).getStatus().toString());
+
+        // make the students submit their answers
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            studentClients.get(i).sendMessage("""
+                {
+                    "action": "ADD_RESULT",
+                    "resultElementId": %s,
+                    "resultValues": ["2"]
+                }
+            """.formatted(course.getQuizForms().get(0).questions.get(0).getId().toString()));
+            Thread.sleep(100);
+        }
+
+        Thread.sleep(15_000);
+
+        // check that the results were added
+        Assertions.assertEquals(AMOUNT_OF_STUDENTS, courseService.getCourse(courseId).getQuizForms().get(0).getQuestions().get(0).getResults().size());
+
+        // check that the score was updated
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            Assertions.assertTrue(courseService.getCourse(courseId).getQuizForms().get(0).getParticipants().get(i).getScore() > 9);
+        }
+
+        // let the prof stop the question
+        profClient.sendMessage("""
+            {
+                "action": "NEXT"
+            }
+        """);
+        Thread.sleep(5000);
+
+        // check that the currentQuestionIndex is 0 and the currentQuestionFinished is true
+        Assertions.assertEquals(0, courseService.getCourse(courseId).getQuizForms().get(0).getCurrentQuestionIndex());
+        Assertions.assertTrue(courseService.getCourse(courseId).getQuizForms().get(0).getCurrentQuestionFinished());
+
+        // check that all students got the "CLOSED_QUESTION" message
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            Assertions.assertEquals("CLOSED_QUESTION", LiveQuizSocketMessage.getByJsonWithForm(studentClients.get(i).getMessageQueue().get(studentClients.get(i).getMessageQueue().size() - 1)).action);
+            Assertions.assertTrue(LiveQuizSocketMessage.getByJsonWithForm(studentClients.get(i).getMessageQueue().get(studentClients.get(i).getMessageQueue().size() - 1)).userHasAnsweredCorrectly);
+        }
+
+        // let the prof start the next question
+        profClient.sendMessage("""
+            {
+                "action": "NEXT"
+            }
+        """);
+        Thread.sleep(1000);
+
+        // check that the students got the opened and closed question message and that it contains the correct result
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            // Assertions.assertEquals("CLOSED_QUESTION", LiveQuizSocketMessage.getByJsonWithForm(studentClients.get(i).getMessageQueue().get(studentClients.get(i).getMessageQueue().size() - 2)).action);
+            // Assertions.assertFalse(LiveQuizSocketMessage.getByJsonWithForm(studentClients.get(i).getMessageQueue().get(studentClients.get(i).getMessageQueue().size() - 2)).userHasAnsweredCorrectly);
+            Assertions.assertEquals("OPENED_NEXT_QUESTION", LiveQuizSocketMessage.getByJsonWithForm(studentClients.get(i).getMessageQueue().get(studentClients.get(i).getMessageQueue().size() - 1)).action);
+        }
+
+        // add a wrong result to the quiz form
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            studentClients.get(i).sendMessage("""
+                {
+                    "action": "ADD_RESULT",
+                    "resultElementId": %s,
+                    "resultValues": ["1"]
+                }
+            """.formatted(course.getQuizForms().get(0).questions.get(1).getId().toString()));
+            Thread.sleep(100);
+        }
+
+        Thread.sleep(5000);
+
+        // check that the results were added
+        Assertions.assertEquals(AMOUNT_OF_STUDENTS, courseService.getCourse(courseId).getQuizForms().get(0).questions.get(1).results.size());
+        Assertions.assertEquals("1", courseService.getCourse(courseId).getQuizForms().get(0).questions.get(1).results.get(0).values.get(0));
+
+        // check that the score was updated
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            Assertions.assertTrue(courseService.getCourse(courseId).getQuizForms().get(0).getParticipants().get(i).getScore() < 16);
+        }
+
+        // close the websocket connections
+        profSession.close();
+        for (int i = 0; i < AMOUNT_OF_STUDENTS; i++) {
+            studentSessions.get(i).close();
+        }
+
+    }
+
 
 }
